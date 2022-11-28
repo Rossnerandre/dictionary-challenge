a0830828315ce61f9fe45c8d9df999787dfd2d99
André Rossner <rossnerandre@gmail.com>
2022-11-28 15:22:52 -0300

    Add Header.tsx

---
+++ src/layout/Header.tsx
@@ -0,0 +1,83 @@
+// @mui
+import {styled, useTheme} from '@mui/material/styles';
+import {Box, AppBar, Toolbar, Container} from '@mui/material';
+// hooks
+import useOffSetTop from '../hooks/useOffSetTop';
+import useResponsive from '../hooks/useResponsive';
+// utils
+import cssStyles from '../utils/cssStyles';
+// config
+import {HEADER} from '../config';
+// components
+import Logo from '../components/Logo';
+//
+import MenuDesktop from './MenuDesktop';
+import MenuMobile from './MenuMobile';
+import navConfig from './MenuConfig';
+
+// ----------------------------------------------------------------------
+
+const ToolbarStyle = styled(Toolbar)(({theme}) => ({
+  height: HEADER.MOBILE_HEIGHT,
+  transition: theme.transitions.create(['height', 'background-color'], {
+    easing: theme.transitions.easing.easeInOut,
+    duration: theme.transitions.duration.shorter,
+  }),
+  [theme.breakpoints.up('md')]: {
+    height: HEADER.MAIN_DESKTOP_HEIGHT,
+  },
+}));
+
+const ToolbarShadowStyle = styled('div')(() => ({
+  left: 0,
+  right: 0,
+  bottom: 0,
+  height: 24,
+  zIndex: -1,
+  margin: 'auto',
+  borderRadius: '50%',
+  position: 'absolute',
+  width: `calc(100% - 48px)`,
+}));
+
+// ----------------------------------------------------------------------
+
+export default function MainHeader() {
+  const isOffset = useOffSetTop(HEADER.MAIN_DESKTOP_HEIGHT);
+
+  const theme = useTheme();
+
+  const isDesktop = useResponsive('up', 'md');
+
+  return (
+    <AppBar sx={{boxShadow: 0, bgcolor: 'primary'}}>
+      <ToolbarStyle
+        disableGutters
+        sx={{
+          ...(isOffset && {
+            ...cssStyles(theme).bgBlur({opacity: 0.2}),
+            height: {md: HEADER.MAIN_DESKTOP_HEIGHT - 16},
+          }),
+        }}
+      >
+        <Container
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Logo/>
+
+          <Box sx={{flexGrow: 1}}/>
+
+          {isDesktop && <MenuDesktop isOffset={isOffset} navConfig={navConfig}/>}
+
+          {!isDesktop && <MenuMobile isOffset={isOffset} navConfig={navConfig}/>}
+        </Container>
+      </ToolbarStyle>
+
+      {isOffset && <ToolbarShadowStyle/>}
+    </AppBar>
+  );
+}
