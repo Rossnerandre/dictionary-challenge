1c57e54160bff0eb599da43f4458bfd0492b2799
André Rossner <rossnerandre@gmail.com>
2022-11-28 15:23:23 -0300

    Add type.ts para os menus

---
+++ src/layout/type.ts
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+
+// ----------------------------------------------------------------------
+
+export type MenuItemProps = {
+  title: string;
+  path: string;
+  icon?: ReactElement;
+  to?: string;
+};
+
+export type MenuProps = {
+  isOffset: boolean;
+  navConfig: MenuItemProps[];
+};
