242eb1bb2833d8ac095e38daa527b47305f3b179
André Rossner <rossnerandre@gmail.com>
2022-11-28 15:23:34 -0300

    Add MenuConfig.tsx

---
+++ src/layout/MenuConfig.tsx
@@ -0,0 +1,18 @@
+import Iconify from '../components/Iconify';
+
+// ----------------------------------------------------------------------
+
+const ICON_SIZE = {
+  width: 22,
+  height: 22,
+};
+
+const menuConfig = [
+  {
+    title: 'Home',
+    icon: <Iconify icon={'eva:home-fill'} {...ICON_SIZE} />,
+    path: '/',
+  },
+];
+
+export default menuConfig;
