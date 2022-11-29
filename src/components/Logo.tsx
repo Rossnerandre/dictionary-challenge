5c5dab224f920a2ca3089e0327d680ea32fc5bf6
André Rossner <rossnerandre@gmail.com>
2022-11-29 16:22:10 -0300

    Correção do caminho da IMG da logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,9 +9,10 @@ export default function Logo({sx}: BoxProps) {
   const logo = (
     <Box
       component="img"
-      src="logoPreto.png"
+      src="/logoPreto.png"
       sx={{width: 100, height: 50, cursor: 'pointer', ...sx}}
     />
+
   );
 
   return <RouterLink to="/">{logo}</RouterLink>;
